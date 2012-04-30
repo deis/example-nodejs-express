b882956b005200f84ceba9bbfaaba6b4f13fa3bd
gabrtv <gabriel@opdemand.com>
2012-04-30 11:48:44 -0600

    try/catch around APPLICATION_PORT lookup

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ app.get('/', function(req, res){
   res.send('Powered by OpDemand');
 });
 
-var port = process.env.PORT
+/* Use APPLICATION_PORT environment variable if it exists */
+try {
+  var port = process.env.APPLICATION_PORT
+} catch(err) {
+  var port = 3000
+}
+
 app.listen(port);
 
 console.log('Server listening on port %d in %s mode', app.address().port, app.settings.env);
