7056969ddf993f7215675ed3602ddd25e5056e12
Gabriel Monroy <gabriel@opdemand.com>
2013-01-12 17:10:05 -0700

    change to PORT envvar

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ app.get('/', function(req, res){
   res.send('Powered by OpDemand');
 });
 
-/* Use APPLICATION_PORT environment variable if it exists */
-var port = process.env.APPLICATION_PORT || 3000;
+/* Use PORT environment variable if it exists */
+var port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('Server listening on port %d in %s mode', app.address().port, app.settings.env);
