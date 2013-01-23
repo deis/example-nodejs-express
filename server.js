3738cdf349908d3df170015582d3f481ab73f234
Gabriel Monroy <gabriel@opdemand.com>
2013-01-22 20:00:49 -0700

    Update server.js
    test9

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express').createServer();
 
 app.get('/', function(req, res){
-  res.send('Powered by OpDemand (test8)');
+  res.send('Powered by OpDemand (test9)');
 });
 
 /* Use PORT environment variable if it exists */
