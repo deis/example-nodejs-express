504ec6fdb6af87796c4e1bdf569de0d55f1b26af
Gabriel Monroy <gabriel@opdemand.com>
2013-03-26 11:35:33 -0600

    update to node 0.10 and express 3

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var app = require('express').createServer();
+var express = require('express');
+var app = express();
 
 app.get('/', function(req, res){
-  res.send('Powered by OpDemand!!!');
+  res.send('Powered by OpDemand!');
 });
 
 /* Use PORT environment variable if it exists */
-var port = process.env.PORT || 3000;
-app.listen(port);
+var port = process.env.PORT || 5000;
+server = app.listen(port);
 
-console.log('Server listening on port %d in %s mode', app.address().port, app.settings.env);
+console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
