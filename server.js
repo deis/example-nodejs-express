c49a92156f67e26967997baadbba8293150f5d40
Gabriel Monroy <gabriel@opdemand.com>
2013-07-19 14:01:18 -0600

    powered by deis!

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Powered by OpDemand!');
+  res.send('Powered by Deis!');
 });
 
 /* Use PORT environment variable if it exists */
