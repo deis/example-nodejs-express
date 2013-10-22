56b1a30b08fc8920a23eee7b2349fc32155c80dd
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-10-22 11:55:04 -0600

    Added environment variable to app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Powered by Deis!');
+  res.send('Powered by ' + process.env.POWERED_BY);
 });
 
 /* Use PORT environment variable if it exists */
