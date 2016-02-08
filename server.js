dcf43915c766515d78b2a0f48e7d29afface9d38
Ken Powers <ken@kenpowers.net>
2016-02-08 11:51:14 -0500

    Put listening log in listen callback.
    `listen` is asynchronous.

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res){
 
 /* Use PORT environment variable if it exists */
 var port = process.env.PORT || 5000;
-server = app.listen(port);
-
-console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
+server = app.listen(port, function () {
+  console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
